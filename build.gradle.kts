9ea6ea40b026f2d1686d1050c8f0b58c4b07637c
Tosin Onikute <tosinmath007@gmail.com>
2024-01-20 15:12:48 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     id("com.android.application") version "8.1.0-beta01" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
+
+println("Value of Secret: ${System.getenv("AWS_CODE_ARTIFACT_DEFAULT_REGION")}")
+println("Value of Env variable: ${System.getenv("SAMPLE")}")
